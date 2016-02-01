0fd97b4ab568263b365c2d014583f315e07e328b
Bär Halberkamp <fledaway@gmail.com>
2016-02-01 20:14:30 +0100

    Add a counter for profanities

---
+++ analyzers/profanities.js
@@ -0,0 +1,33 @@
+var profanityList = [
+    'fuck', 'shit', 'cock', 'dick', 'pussy', 'roastie', 'cunt',
+    'autist', 'aspie', 'retard', 'cuck', 'cuckold',
+    'nigger', 'fag', 'faggot', 'meanie', 'poopyhead', 'kike', 'spic', 'sandnigger', 'pinoy', 'nigga', 'mexican'
+];
+
+module.exports = {
+    parser: function(room, message) {
+        // Don't even bother with messages that are just emoticons.
+        if (toId(message).length < 2) return false;
+
+        var words = message.split(' ');
+
+        var profanities = 0;
+        for (var i = 0; i < words.length; i++) {
+            if (profanityList.indexOf(words[i].toLowerCase()) > -1) {
+                profanities++;
+            }
+        }
+
+        if (!Data[room]) Data[room] = {};
+        if (!Data[room].profanities) {
+            Data[room].profanities = {count: profanities, total: words.length};
+        } else {
+            Data[room].profanities.count += profanities;
+            Data[room].profanities.total += words.length;
+        }
+
+        Handler.writeData();
+    },
+
+    display: room => 'Percentage of words said that are swear words: ' + (Data[room].profanities ? Data[room].profanities.count / Data[room].profanities.total * 100 : 0),
+}
