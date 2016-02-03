44c43f70fe7a90d7f50bdca5744604e3af93ffe4
Bär Halberkamp <fledaway@gmail.com>
2016-02-03 14:23:00 +0100

    allow analyzers without a display function

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function generateRoomPage(req, res) {
 	var room = req.originalUrl.split('/')[1];
 	var content = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="style.css"><title>' + room + ' - Kid A</title></head><body>'
 	for (var i in Handler.analyzers) {
-		if (!Handler.analyzers[i].rooms || Handler.analyzers[i].rooms.indexOf(room) > -1) {
+		if (Handler.analyzers[i].display && (!Handler.analyzers[i].rooms || Handler.analyzers[i].rooms.indexOf(room) > -1)) {
 			content += Handler.analyzers[i].display(room) + '<br/></br/>';
 		}
 	}
