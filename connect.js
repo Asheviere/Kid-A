a169a88a3bef228d1b34e5ae212266fb1a43c365
Bär Halberkamp <fledaway@gmail.com>
2016-02-09 17:31:39 +0100

    Update this as well

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 var WebSocketClient = require('websocket').client;
 
-var retryTime = 5; // Time (in seconds) before the bot retries a failed connection.
+var retryTime = 10; // Time (in seconds) before the bot retries a failed connection.
 
 var con;
 
@@ -8,19 +8,19 @@ function connect() {
     var client = new WebSocketClient();
 
     client.on('connectFailed', error => {
-        console.log('Connection failed with error: ' + error + ". Retrying in " + retryTime + "s.");
+        errorMsg('Connection failed with error: ' + error + ". Retrying in " + retryTime + "s.");
         setTimeout(connect, retryTime * 1000);
     });
 
     client.on('connect', connection => {
         Connection = connection;
-        console.log('WebSocket Client Connected');
+        statusMsg('WebSocket Client Connected');
         connection.on('error', error => {
-            console.log("Error on connection" + connection + ": " + error + ". Reconnecting in " + retryTime + "s.");
+            errorMsg(error + ". Reconnecting in " + retryTime + "s.");
             setTimeout(connect, retryTime * 1000);
         });
         connection.on('close', () => {
-            console.log("Closed connection " + connection + ", reconnecting in " + retryTime + "s.");
+            statusMsg("Closed connection, reconnecting in " + retryTime + "s.");
             setTimeout(connect, retryTime * 1000);
         });
         connection.on('message', message => {
@@ -28,7 +28,7 @@ function connect() {
         });
     });
 
-    console.log("Connecting...");
+    statusMsg("Connecting...");
     client.connect('ws://' + Config.host + ':' + Config.port + '/showdown/websocket');
 }
 
