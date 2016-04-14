17057ec745555a54e8299a190f1854810c632060
Bär Halberkamp <fledaway@gmail.com>
2016-04-14 22:57:02 +0200

    Invert order of rooms and function in urls

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var site = connect();
 site.use(serveStatic(__dirname + '/public'));
 
 function generateRoomPage(req, res) {
-	var room = req.originalUrl.split('/')[2];
+	var room = req.originalUrl.split('/')[1];
 	var content = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="style.css"><title>' + room + ' - Kid A</title></head><body>'
 	for (var i in Handler.analyzers) {
 		if (Handler.analyzers[i].display && (!Handler.analyzers[i].rooms || Handler.analyzers[i].rooms.indexOf(room) > -1)) {
@@ -21,7 +21,7 @@ function generateRoomPage(req, res) {
 }
 
 function generateQuotePage(req, res) {
-	var room = req.originalUrl.split('/')[2];
+	var room = req.originalUrl.split('/')[1];
 	var content = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="style.css"><title>' + room + ' - Kid A</title></head><body>'
 	if (Data.quotes[room]) {
 		for (var i = 0; i < Data.quotes[room].length; i++) {
@@ -33,11 +33,11 @@ function generateQuotePage(req, res) {
 }
 
 for (var room in Data.data) {
-	site.use('/data/' + room, generateRoomPage);
+	site.use('/' + room + '/data', generateRoomPage);
 }
 
 for (var room in Data.quotes) {
-	site.use('/quotes/' + room, generateQuotePage);
+	site.use('/' + room + '/quotes', generateQuotePage);
 }
 
 site.use((req, res) => res.end("Invalid room."));
