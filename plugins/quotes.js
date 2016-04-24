1d6515adcb20a0895828d77e64e56433958ceaf3
Bär Halberkamp <fledaway@gmail.com>
2016-04-24 15:24:36 +0200

    Add a .quotes command

--- plugins/quotes.js
+++ plugins/quotes.js
@@ -16,5 +16,15 @@ module.exports = {
 
             Handler.writeQuotes();
         },
+        quotes: function (symbol, room, message) {
+            if (!room) return {pmreply: "This command can't be used in PMs."};
+            if (!canUse(symbol, 1)) return {pmreply: "Permission denied."};
+
+            if (Data.quotes[room]) {
+                return {reply: "http://" + Config.serverhost + ":" + Config.serverport + "/" + room + "/quotes"};
+            } else {
+                return {pmreply: "This room has no quotes."};
+            }
+        },
     },
 };
