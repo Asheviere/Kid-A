afdcfec0324fadc61517636e2a3ffe0f6c05cec0
Bär Halberkamp <fledaway@gmail.com>
2016-05-11 21:22:32 +0200

    Allow users to register their lastfm username

--- plugins/lastfm.js
+++ plugins/lastfm.js
@@ -1,5 +1,23 @@
 var request = require('request');
 
+function loadLastfmData() {
+	var data;
+	try {
+		data = require('../data/lastfm.json');
+	} catch (e) {}
+
+	if (!Object.isObject(data)) data = {};
+
+	return data;
+}
+
+function writeLastfmData() {
+	var toWrite = JSON.stringify(Data.lastfm);
+	fs.writeFileSync('./data/lastfm.json', toWrite);
+}
+
+Databases.addDatabase('lastfm', loadLastfmData, writeLastfmData);
+
 var API_ROOT = 'http://ws.audioscrobbler.com/2.0/';
 
 module.exports = {
@@ -8,6 +26,10 @@ module.exports = {
             if (!canUse(userstr, 1)) return {pmreply: "Permission denied."};
 
             if (!Config.lastfmKey) return errorMsg("No last.fm API key found.");
+
+            var userid = toId(userstr);
+            var accountname = message || userstr.substr(1);
+            if (!message && (userid in Data.lastfm)) message = Data.lastfm[userid];
 
             var url = API_ROOT + '?method=user.getrecenttracks&user=' + message + '&limit=1&api_key=' + Config.lastfmKey + '&format=json';
             var req = new Promise(function(resolve, reject) {
@@ -24,7 +46,7 @@ module.exports = {
             return req.then(data => {
                 var msg = '';
                 if (data.recenttracks && data.recenttracks.track && data.recenttracks.track.length) {
-                    msg += message;
+                    msg += accountname;
                     var track = data.recenttracks.track[0];
                     if (track['@attr'] && track['@attr'].nowplaying) {
                         msg += " is now listening to: ";
@@ -43,6 +65,16 @@ module.exports = {
                 }
                 return {reply: msg};
             }, data => {pmreply: "Something went wrong! Please try again, or contact the bot's administator(s) when this problem persists."});
+        },
+        registerlastfm: function(userstr, room, message) {
+            if (!message) return {pmreply: "No username entered."};
+
+            var userid = toId(userstr);
+            var username = message.replace(/[^A-Za-z0-9-_]/g, '');
+
+            Data.lastfm[userid] = username;
+
+            Databases.writeDatabase('lastfm');
         }
     },
 };
