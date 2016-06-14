1ed8ea6b31ac346786cd55ca0a87e87e3062b028
Bär Halberkamp <fledaway@gmail.com>
2016-06-14 17:44:29 +0200

    update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,9 +1,9 @@
 exports.host = 'sim.smogon.com';
-exports.port = '8000';
+exports.port = 8000;
 
 // Host and port to use for the http server part of Kid A.
 exports.serverhost = 'localhost';
-exports.serverport = '8000';
+exports.serverport = 8000;
 
 // Username and password to use on PS.
 exports.username = '';
@@ -12,7 +12,7 @@ exports.password = '';
 // Rooms to join and avatar to choose. The maximum amount of rooms Kid A can join upon connecting is 11.
 // The reason for these restrictions is the way PS protocol works. I might try to get around it at a later date, but this is it for now.
 exports.rooms = ['dev'];
-exports.avatar = '246';
+exports.avatar = 246;
 
 // Symbol to use for commands.
 exports.commandSymbol = '.';
