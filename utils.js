0f9b847943d00c05c55c3b890539114a1496db2f
Bär Halberkamp <fledaway@gmail.com>
2016-06-23 10:10:37 -0400

    .txt -> .html

--- utils.js
+++ utils.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 module.exports = {
 	generateTempFile(content, time) {
 		let filename = crypto.randomBytes(10).toString('hex');
-		let path = './public/' + filename + '.txt';
+		let path = './public/' + filename + '.html';
 		fs.writeFileSync(path, content);
 		setTimeout(() => fs.unlinkSync(path), 1000 * 60 * time);
-		return filename + '.txt';
+		return filename + '.html';
 	},
 };
