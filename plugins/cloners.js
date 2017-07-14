283f6d252185bcdbbbba54e8980dcc5550d75aa7
Bär Halberkamp <fledaway@gmail.com>
2017-07-14 16:45:14 -0400

    fix sorting again

--- plugins/cloners.js
+++ plugins/cloners.js
@@ -114,15 +114,21 @@ class WifiList {
 
 			if (this.columnKeys.includes('score')) {
 				top3 = Object.keys(this.data).sort((a, b) => {
-					return this.data[a].score >= this.data[b].score;
+					return (parseInt(this.data[a].score) > parseInt(this.data[b].score) ? -1 : 1);
 				}).slice(0, 3);
 			}
 
 			data.entries = Object.keys(this.data).sort((a, b) => {
 				if ('date' in this.data[a] && !parseInt(this.data[a].date)) return -1;
 				if ('date' in this.data[b] && !parseInt(this.data[b].date)) return 1;
-				if (top3.indexOf(a) < top3.indexOf(b)) return -1;
-				if (top3.indexOf(b) < top3.indexOf(a)) return 1;
+				if (top3.indexOf(a) !== -1) {
+					if (top3.indexOf(b) !== -1) {
+						return (top3.indexOf(a) < top3.indexOf(b) ? -1 : 1);
+					}
+					return -1;
+				} else if (top3.indexOf(b) !== -1) {
+					return 1;
+				}
 				return a.localeCompare(b);
 			}).map(val => ({data: this.data[val], online: (!this.noOnlinePage && (Handler.userlists[WIFI_ROOM] && (val in Handler.userlists[WIFI_ROOM] || (this.data[val].alts && this.data[val].alts.split(',').map(val => toId(val)).filter(val => val in Handler.userlists[WIFI_ROOM]).length))))}));
 
@@ -285,7 +291,7 @@ class WifiList {
 	updateScore(userid) {
 		if (!(this.data[userid] && ('score' in this.data[userid]))) return;
 
-		this.data[userid].score++;
+		this.data[userid].score = parseInt(this.data[userid].score) + 1;
 
 		this.writeList();
 	}
