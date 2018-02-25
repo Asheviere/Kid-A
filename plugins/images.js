bf65bf30374b75d81eae1aaafa04b9a0d208c212
Bär Halberkamp <fledaway@gmail.com>
2018-02-25 19:05:16 +0100

    Change ordering

--- plugins/images.js
+++ plugins/images.js
@@ -130,9 +130,9 @@ module.exports = {
 					pendingApprovals.delete(this.room);
 					return this.reply(`/addhtmlbox <table><tbody><tr><td style="padding-right: 5px"><img src="${data.thumbnail}" width="120" height="90"></td><td><b><a href=${VIDEO_ROOT}${data.id}>${data.title}</a></b><br/>Uploaded ${data.date.toDateString()} by <b><a href="${CHANNEL_ROOT}${data.channelUrl}">${data.channel}</a></b><br/><b>${data.views}</b> views, <b><span style="color:green">${data.likes}</span> | <span style="color:red">${data.dislikes}</span></b><br/><details><summary>[Description]</summary><i>${data.description.replace(/\n/g, '<br/>')}</i></details></td></tr></tbody></table>`);
 				default:
+					pendingApprovals.delete(this.room);
 					let [width, height] = await fitImage(data).catch(() => this.reply("Something went wrong getting dimensions of the image."));
 
-					pendingApprovals.delete(this.room);
 					if (!(width && height)) return;
 					return this.reply(`/addhtmlbox <a href="${data}"><img src="${data}" width="${Math.round(width)}" height="${Math.round(height)}"/></a><br/><small>(Image suggested by ${user} and approved by ${this.username})</small>`);
 				}
