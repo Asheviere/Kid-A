734837c34da7d69c0c370f7bab586fd6cc987fb5
Bär Halberkamp <fledaway@gmail.com>
2018-02-27 22:13:08 +0100

    Add descriptions to approvals

--- plugins/approvals.js
+++ plugins/approvals.js
@@ -13,6 +13,12 @@ const HOUR = 60 * 60 * 1000;
 
 const settings = redis.useDatabase('settings');
 
+// Thanks Zarel for this obviously extremely well-coded function from PS.
+function escapeHTML(str) {
+	if (!str) return '';
+	return ('' + str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;').replace(/\//g, '&#x2f;');
+}
+
 async function fitImage(url, maxHeight = 300, maxWidth = 400) {
 	let {height, width} = await probe(url);
 
@@ -59,15 +65,15 @@ const ROOMS = [COSMO, YOUTUBE_ROOM];
 
 const selfLinkTimeouts = new Map();
 
-async function draw(user, data, self) {
+async function draw(user, data, desc, self) {
 	switch (this.room) {
 	case YOUTUBE_ROOM:
-		return this.reply(`/addhtmlbox <table><tbody><tr><td style="padding-right: 5px"><img src="${data.thumbnail}" width="120" height="90"></td><td><b><a href=${VIDEO_ROOT}${data.id}>${data.title}</a></b><br/>Uploaded ${data.date.toDateString()} by <b><a href="${CHANNEL_ROOT}${data.channelUrl}">${data.channel}</a></b><br/><b>${data.views}</b> views, <b><span style="color:green">${data.likes}</span> | <span style="color:red">${data.dislikes}</span></b><br/><details><summary>[Description]</summary><i>${data.description.replace(/\n/g, '<br/>')}</i></details></td></tr></tbody></table>`);
+		return this.reply(`/addhtmlbox <table><tbody><tr><td style="padding-right: 5px"><img src="${data.thumbnail}" width="120" height="90"></td><td><b><a href=${VIDEO_ROOT}${data.id}>${data.title}</a></b><br/>Uploaded ${data.date.toDateString()} by <b><a href="${CHANNEL_ROOT}${data.channelUrl}">${data.channel}</a></b><br/><b>${data.views}</b> views, <b><span style="color:green">${data.likes}</span> | <span style="color:red">${data.dislikes}</span></b><br/>${desc ? `<i>${escapeHTML(desc)}</i><br/>` : ''}<details><summary>[Video Description]</summary><i>${escapeHTML(data.description).replace(/\n/g, '<br/>')}</i></details></td></tr></tbody></table>`);
 	default:
 		let [width, height] = await fitImage(data).catch(() => this.reply("Something went wrong getting dimensions of the image."));
 
 		if (!(width && height)) return;
-		return this.reply(`/addhtmlbox <a href="${data}"><img src="${data}" width="${Math.round(width)}" height="${Math.round(height)}"/></a>${self ? "" : `<br/><small>(Image suggested by ${user} and approved by ${this.username})</small>`}`);
+		return this.reply(`/addhtmlbox <a href="${data}"><img src="${data}" width="${Math.round(width)}" height="${Math.round(height)}"/></a>${desc ? `<i>${escapeHTML(desc)}</i><br/>` : ""}${self ? "" : `<br/><small>(Image suggested by ${user} and approved by ${this.username})</small>`}`);
 	}
 }
 
@@ -116,27 +122,28 @@ module.exports = {
 			rooms: ROOMS,
 			async action(message) {
 				let room = this.room;
-				let url;
+				let url, description;
 				if (!room) {
-					let split = message.split(',');
-					[room, url] = split.map(param => param.trim());
-					if (!(room && url)) return this.pmreply("Syntax: ``.requestapproval room, url``");
+					[room, url, description] = message.split(',').map(param => param.trim());
+					if (!(room && url)) return this.pmreply("Syntax: ``.requestapproval room, url, (optional) description``");
 					if (!ROOMS.includes(room)) return this.pmreply("This room does not support this feature.");
 					if (!this.getRoomAuth(room)) return;
 				} else {
-					url = message;
-					if (!url) return this.pmreply("Syntax: ``.requestapproval url``");
+					[url, description] = message.split(',').map(param => param.trim());
+					if (!url) return this.pmreply("Syntax: ``.requestapproval url, (optional) description``");
 				}
 
 				if (pendingApprovals.has(room)) return this.reply("There is already someone awaiting approval.");
+				if (description && description.length > 200) return this.reply("The description is too long.");
 
 				let data = await parse.call(this, room, url);
 				if (!data) return;
+				if (description) data.description = description;
 
 				pendingApprovals.set(room, data);
 
 				Connection.send(`${room}|${this.username} wishes to have a link approved!`);
-				Connection.send(`${room}|/modnote ${this.username} wishes to get approval to post '${url}' in the room. Type .approve or .reject to handle the request.`);
+				Connection.send(`${room}|/modnote ${this.username} wishes to get approval to post '${url}' in the room${description ? ` (${description})` : ''}. Type .approve or .reject to handle the request.`);
 			},
 		},
 		approve: {
@@ -147,9 +154,9 @@ module.exports = {
 			async action() {
 				if (!pendingApprovals.has(this.room)) return this.pmreply("There is nothing to approve.");
 
-				let {user, data} = pendingApprovals.get(this.room);
+				let {user, data, description} = pendingApprovals.get(this.room);
 				pendingApprovals.delete(this.room);
-				await draw.call(this, user, data);
+				await draw.call(this, user, data, description);
 			},
 		},
 		link: {
@@ -163,9 +170,11 @@ module.exports = {
 					if (selfLinkTimeouts.has(this.userid)) return this.reply("You are only allowed to post your own link once per two hours.");
 					selfLinkTimeouts.set(this.userid, setTimeout(() => selfLinkTimeouts.delete(this.userid), 2 * HOUR));
 				}
-				let {user, data} = await parse.call(this, this.room, message);
+				let [url, description] = message.split(',').map(param => param.trim());
+				if (description && description.length > 200) return this.reply("The description is too long.");
+				let {user, data} = await parse.call(this, this.room, url);
 				if (!user) return;
-				draw.call(this, this.username, data, true);
+				draw.call(this, this.username, data, description, true);
 			},
 		},
 		reject: {
