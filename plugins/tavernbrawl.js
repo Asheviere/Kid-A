55bc06f0d322225b544fd603461a66d9278baa6f
Bär Halberkamp <fledaway@gmail.com>
2018-04-13 15:17:07 +0200

    Hearthstone rotation

--- plugins/tavernbrawl.js
+++ plugins/tavernbrawl.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const TAVERN_BRAWL = 'tavernbrawl';
 const QUERYURL = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/';
-const STANDARD_SETS = ['Basic', 'Classic', 'Whispers of the Old Gods', 'One Night in Karazhan', 'Mean Streets of Gadgetzan', 'Journey to Un\'Goro', 'Knights of the Frozen Throne', 'Kobolds & Catacombs'];
+const STANDARD_SETS = ['Basic', 'Classic', 'Journey to Un\'Goro', 'Knights of the Frozen Throne', 'Kobolds & Catacombs', 'The Witchwood'];
 
 function sanitizeCardText(text) {
 	return text.replace(/\[x\]/g, '').replace(/\\n/g, '<br/>').replace(/\n/g, '<br/>').replace(/_/g, ' ').replace(/\$/g, '');
