caca2c369c52ba1621437ed91abe229d307780c6
Bär Halberkamp <fledaway@gmail.com>
2018-09-11 13:11:01 +0200

    Fix mistake in debug level detection

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ require('./utils.js');
  * 4: Also including database
  * 5: Absolutely everything
  */
-if (process.argv.length > 1) {
-	if (process.argv[1] === 'debug') {
+if (process.argv.length > 2) {
+	if (process.argv[2] === 'debug') {
 		let logLvl = 2;
-		if (process.argv.length > 2) {
-			logLvl = parseInt(process.argv[2]) || logLvl;
+		if (process.argv.length > 3) {
+			logLvl = parseInt(process.argv[3]) || logLvl;
 			if (logLvl > 5) logLvl = 5;
 		}
 		Output.log('status', `Loading Debug Mode with log level ${logLvl}`);
