292d7a7d86021b5197ef01358e1e2e1a3d84b180
fart <24866009+tmagicturtle@users.noreply.github.com>
2020-05-25 19:29:23 -0400

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -497,7 +497,7 @@ module.exports = {
 				}
 
 				for (let i = 0; i < output.length; i += 5) {
-					cols.push(output.slice(i, i + 5).join('<br/>'));
+					cols.push(output.slice(i, i + 5).join('<br>'));
 				}
 
 				return this.replyHTML(`<div style="width:100%;overflow-x:auto;display:inline-flex;">${cols.map(col => `<div style="margin:auto 5px auto 0px;${this.pm && !col.startsWith('<img') ? 'min-width:155px;max-width:155px;' : ''}">${col}</div>`).join('')}</div>`);
